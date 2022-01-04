37ee6035b88f6372819df4c3c5cbcfd7d7e344da
Roland Krasser <roland.krasser@gmail.com>
2022-01-04 09:54:00 +0100

    add create_fake_data

--- R/tools.R
+++ R/tools.R
@@ -871,3 +871,134 @@ count_pct <- function(data, ...)  {
 } # count_pct
 
 
+#============================================================================
+#  create_fake_data
+#============================================================================
+#' Create fake data
+#'
+#' Fake data that can be used for unit-testing or demonstration
+#'
+#' @param obs Number of observations
+#' @param target_name Variable name of target
+#' @param factorise_target Should target variable be factorised?
+#' (from 0/1 to facotr no/yes)?
+#' @param target1_prob Probability that buy = 1
+#' @param add_extreme Add an obervation with extreme values?
+#' @param flip_gender Should Male/Female be flipped in data?
+#' @param seed Seed for randomization
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @md
+#' @details
+#' Variables in dataset:
+#' * id = Identifier
+#' * period = Year & Month (YYYYMM)
+#' * city_ind = Indicating if customer is residing in a city (1 = yes, 0 = no)
+#' * female_ind = Gender of customer is female (1 = yes, 0 = no)
+#' * fixedvoice_ind = Customer has a fixed voice product (1 = yes, 0 = no)
+#' * fixeddata_ind = Customer has a fixed data product (1 = yes, 0 = no)
+#' * fixedtv_ind = Customer has a fixed tv product (1 = yes, 0 = no)
+#' * mobilevoice_ind = Customer has a mobile voice product (1 = yes, 0 = no)
+#' * mobiledata_ind = Customer has a mobile data product (1 = yes, 0 = no)
+#' * bbi_speed_ind = Customer has a Broadband Internet (BBI) with speed
+#' * bbi_usg_gb = Broadband Internet (BBI) usage in Gigabyte (GB) last month
+#' * hh_single = Expected to be a Single Household (1 = yes, 0 = no)
+#'
+#' Target in dataset:
+#' * target_ind (may be renamed) = Did customer buy a new product in next month?
+#' (1 = yes, 0 = no)
+
+
+create_fake_data = function(obs = 1000,
+                    target_name = "target_ind",
+                    factorise_target = FALSE,
+                    target1_prob = 0.5,
+                    add_extreme = TRUE,
+                    flip_gender = FALSE,
+                    seed = 123) {
+
+  # set seed (randomization)
+  set.seed(seed)
+
+  # create basic dataset
+  data <- data.frame(
+    id = seq(from = 1, to = obs),
+    period = rep(202012, obs),
+    target_ind = sample(c(0, 1),
+                        obs,
+                        prob = c(1 - target1_prob, target1_prob),
+                        replace = TRUE)
+  )
+
+  # add features
+  data <- data %>%
+    dplyr::mutate(
+      age = round(ifelse(target_ind == 1,
+                         rnorm(obs, mean = 45, sd = 10),
+                         rnorm(obs, mean = 60, sd = 10)
+      ), 0),
+      city_ind = ifelse(target_ind == 1,
+                        sample(c(0, 1), obs, replace = TRUE, prob = c(0.4, 0.6)),
+                        sample(c(0, 1), obs, replace = TRUE, prob = c(0.6, 0.4))
+      ),
+      female_ind = ifelse(target_ind == 1,
+                          sample(c(0, 1), obs, replace = TRUE, prob = c(0.3, 0.7)),
+                          sample(c(0, 1), obs, replace = TRUE, prob = c(0.7, 0.3))
+      ),
+      fixedvoice_ind = ifelse(age > 70,
+                              sample(c(0, 1), obs, replace = TRUE, prob = c(0.3, 0.7)),
+                              sample(c(0, 1), obs, replace = TRUE, prob = c(0.95, 0.05))
+      ),
+      fixeddata_ind = 1,
+      fixedtv_ind = ifelse(target_ind == 1,
+                           sample(c(0, 1), obs, replace = TRUE, prob = c(0.4, 0.6)),
+                           sample(c(0, 1), obs, replace = TRUE, prob = c(0.8, 0.2))
+      ),
+      mobilevoice_ind = sample(c(0, 1), obs, replace = TRUE, prob = c(0.4, 0.6)),
+      mobiledata_ind = sample(c("NO","MOBILE STICK", "BUSINESS"), obs, replace = TRUE, prob = c(0.5, 0.3, 0.2)),
+      bbi_speed_ind = ifelse(age > 60,
+                             sample(c(0, 1), obs, replace = TRUE, prob = c(0.9, 0.1)),
+                             sample(c(0, 1), obs, replace = TRUE, prob = c(0.2, 0.8))
+      ),
+      bbi_usg_gb = ifelse(age > 75,
+                          round(rnorm(obs, mean = 10, sd = 1)),
+                          round(rnorm(obs, mean = 50, sd = 10))
+      ) + city_ind * 20 + target_ind * 10,
+      hh_single = ifelse(age < 35 & city_ind == 1,
+                         sample(c(0, 1), obs, replace = TRUE, prob = c(0.2, 0.8)),
+                         sample(c(0, 1), obs, replace = TRUE, prob = c(0.7, 0.3))
+      )
+    ) # mutate
+
+  # factorise target?
+  if (factorise_target) {
+    data$buy <- factor(data$buy,
+                       levels = c(0, 1),
+                       labels = c("no", "yes"))
+  }
+
+  # add extreme values?
+  if (add_extreme) {
+    extreme <- data[nrow(data), ]
+    extreme$bbi_usg_gb <- 100000
+    extreme$target_ind <- 0
+    data[nrow(data), ] <- extreme[1, ]
+  }
+
+  # rename target?
+  if (target_name != "target_ind") {
+    data[[target_name]] <- data$target_ind
+    data$target_ind <- NULL
+  }
+
+  # flip gender?
+  if (flip_gender) {
+    data[["female_ind"]] <- ifelse(data[["female_ind"]] == 1, 0, 1)
+  }
+
+  # return data
+  data
+
+} # fakedata
