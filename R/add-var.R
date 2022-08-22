0598adce6b2a2a103752ecb615cb110fe91d09ce
Roland Krasser <roland.krasser@gmail.com>
2022-08-22 17:07:35 +0200

    add_var_ create_data_

---
+++ R/add-var.R
@@ -0,0 +1,268 @@
+#' Add a random categorical variable to dataset
+#'
+#' @param data A dataset
+#' @param name Name of new variable (as string)
+#' @param cat Vector of categories
+#' @param prob Vector of probabilities
+#' @param overwrite Can new random variable overwrite an existing variable in dataset?
+#' @param seed Seed for random number generation (integer)
+#' @return Dataset containing new random variable
+#' @examples
+#' add_var_random_cat(iris)
+#' add_var_random_cat(iris, name = "my_cat")
+#' add_var_random_cat(iris, cat = c("Version A", "Version B"))
+#' add_var_random_cat(iris, cat = c(1,2,3,4,5))
+#' @export
+
+add_var_random_cat <- function(data,
+                               name = "random_cat",
+                               cat = LETTERS[1:6],
+                               prob,
+                               overwrite = TRUE,
+                               seed = 123) {
+
+  # data table available?
+  if (missing(data))  {
+    stop("data is missing")
+  }
+
+  if (!is.data.frame(data)) {
+    stop("expect a table of type data.frame")
+  }
+
+  # check if var already exists
+  if (name %in% names(data) & !overwrite) {
+    stop("Variable ", name, " already exists!")
+  }
+
+  # check prob
+  if (missing(prob)) {
+    prob <- rep(1, length(cat))
+  }
+  diff <- length(cat) - length(prob)
+  if (diff > 0) {
+    prob <- c(prob, rep(0, diff))
+  }
+
+  # check seed
+  if (!missing(seed)) {
+    set.seed(seed)
+  }
+
+  # create var
+  data[[name]] <- sample(cat,
+                         size = nrow(data),
+                         prob = prob,
+                         replace = TRUE)
+
+  # return data
+  data
+} # add_var_random_cat
+
+
+#' Add a random 0/1 variable to dataset
+#'
+#' @param data A dataset
+#' @param name Name of new variable (as string)
+#' @param prob Vector of probabilities
+#' @param overwrite Can new random variable overwrite an existing variable in dataset?
+#' @param seed Seed for random number generation (integer)
+#' @return Dataset containing new random variable
+#' @examples
+#' add_var_random_01(iris)
+#' add_var_random_01(iris, name = "my_var")
+#' @export
+
+add_var_random_01 <- function(data, name = "random_01", prob = c(0.5, 0.5), overwrite = TRUE, seed) {
+
+  data <- add_var_random_cat(data,
+                             name = name,
+                             cat = c(0, 1),
+                             prob = prob,
+                             overwrite = overwrite,
+                             seed = seed)
+  data
+
+} # add_var_random_01
+
+#' Add a random integer variable to dataset
+#'
+#' @param data A dataset
+#' @param name Name of new variable (as string)
+#' @param min_val Minimum random integers
+#' @param max_val Maximum random integers
+#' @param overwrite Can new random variable overwrite an existing variable in dataset?
+#' @param seed Seed for random number generation (integer)
+#' @return Dataset containing new random variable
+#' @examples
+#' add_var_random_int(iris)
+#' add_var_random_int(iris, name = "random_var")
+#' add_var_random_int(iris, min_val = 1, max_val = 10)
+#' add_var_random_int(iris, min_val = 1, max_val = 100, overwrite = FALSE)
+#' @export
+
+add_var_random_int <- function(data, name = "random_int",
+                               min_val = 1, max_val = 10,
+                               overwrite = TRUE,
+                               seed) {
+
+  # data table available?
+  if (missing(data))  {
+    stop("data is missing")
+  }
+
+  if (!is.data.frame(data)) {
+    stop("expect a table of type data.frame")
+  }
+
+  if (name %in% names(data) & !overwrite) {
+    stop("Variable ", name, " already exists!")
+  }
+
+  # check seed
+  if (!missing(seed)) {
+    set.seed(seed)
+  }
+
+  # possible integer values
+  values <- seq(from = min_val, to = max_val)
+
+  # add random variable
+  data[[name]] <- sample(values, nrow(data), replace = TRUE)
+
+  # return data
+  data
+
+} # add_var_random_int
+
+
+#' Add a random double variable to dataset
+#'
+#' @param data A dataset
+#' @param name Name of new variable (as string)
+#' @param min_val Minimum random integers
+#' @param max_val Maximum random integers
+#' @param overwrite Can new random variable overwrite an existing variable in dataset?
+#' @param seed Seed for random number generation (integer)
+#' @return Dataset containing new random variable
+#' @examples
+#' add_var_random_dbl(iris)
+#' add_var_random_dbl(iris, name = "random_var")
+#' add_var_random_dbl(iris, min_val = 1, max_val = 10)
+#' add_var_random_dbl(iris, min_val = 1, max_val = 100, overwrite = FALSE)
+#' @export
+
+add_var_random_dbl <- function(data, name = "random_dbl",
+                               min_val = 0, max_val = 100,
+                               overwrite = TRUE,
+                               seed) {
+
+  # data table available?
+  if (missing(data))  {
+    stop("data is missing")
+  }
+
+  # is it a data.frame?
+  if (!is.data.frame(data)) {
+    stop("expect a table of type data.frame")
+  }
+
+  # check variable name
+  if (name %in% names(data) & !overwrite) {
+    stop("Variable ", name, " already exists!")
+  }
+
+  # check seed
+  if (!missing(seed)) {
+    set.seed(seed)
+  }
+
+  # add random variable
+  data[[name]] <- runif(nrow(data), min = min_val, max = max_val)
+
+  # return data
+  data
+
+} # add_var_random_dbl
+
+
+#' Add a random starsign variable to dataset
+#'
+#' @param data A dataset
+#' @param name Name of new variable (as string)
+#' @param lang Language used for starsign (en = English, de = Deutsch, es = Espanol)
+#' @param overwrite Can new random variable overwrite an existing variable in dataset?
+#' @param seed Seed for random number generation (integer)
+#' @return Dataset containing new random variable
+#' @examples
+#' add_var_random_starsign(iris)
+#' @export
+
+add_var_random_starsign <- function(data, name = "random_starsign", lang = "en", overwrite = TRUE, seed) {
+
+  # add starsign
+  signs <- c("Aries", "Taurus", "Gemini", "Cancer", "Leo",
+             "Virgo", "Libra", "Scorpio", "Saggitarius",
+             "Carpicorn", "Aquarius", "Pisces")
+
+  if (lang == "de") {
+    signs <- c("Widder", "Stier", "Zwilling", "Krebs", "Loewe",
+               "Jungfrau", "Waage", "Skorpion", "Schuetze",
+               "Steinbock", "Wassermann","Fische")
+  }
+
+  if (lang == "es") {
+    signs <- c("Aries", "Tauro", "Geminis", "Cancer", "Leo",
+               "Virgo", "Libra", "Escorpio", "Sagitario",
+               "Capricornio", "Acuario", "Piscis")
+  }
+
+  data <- add_var_random_cat(data,
+                             cat = signs,
+                             name = name,
+                             overwrite = overwrite,
+                             seed = seed)
+  # return data
+  data
+
+} # add_var_random_starsign
+
+
+#' Add a variable id at first column in dataset
+#'
+#' @param data A dataset
+#' @param name Name of new variable (as string)
+#' @param overwrite Can new id variable overwrite an existing variable in dataset?
+#' @return Dataset containing new id variable
+#' @examples
+#' add_var_id_starsign(iris)
+#' @export
+
+add_var_id <- function(data, name = "id", overwrite = FALSE)  {
+
+  # data table available?
+  if (missing(data))  {
+    stop("data is missing")
+  }
+
+  # is it a data.frame?
+  if (!is.data.frame(data)) {
+    stop("expect a table of type data.frame")
+  }
+
+  # check variable name
+  if (name %in% names(data) & !overwrite) {
+    stop("Variable ", name, " already exists!")
+  }
+
+  # drop var if overwrite == TRUE
+  if (name %in% names(data) & overwrite) {
+    data[[name]] <- NULL
+  }
+
+  # create id
+  data <- data.frame(seq(from = 1, to = nrow(data)), data)
+  names(data)[1] <- name
+  data
+
+} # add_var_id
---
+++ R/create-data.R
@@ -0,0 +1,298 @@
+#' Create an empty dataset
+#'
+#' @param obs Number of observations
+#' @param add_id Add an id
+#' @return Dataset
+#' @examples
+#' create_data_empty()
+#' @export
+
+create_data_empty <- function(obs = 1000, add_id = FALSE) {
+
+  data <- data.frame(
+    row.names = seq(1, obs)
+  )
+
+  if (add_id)  {
+    data[["id"]] <- seq(1, obs)
+  }
+
+  data
+
+} # create_data_empty
+
+
+#' Create data person
+#'
+#' Artificial data that can be used for unit-testing or teaching
+#'
+#' @param obs Number of observations
+#' @param seed Seed for randomization (integer)
+#'
+#' @return A dataframe
+#' @export
+
+create_data_person <- function(obs = 1000, seed = 123) {
+
+  # reproducible random numbers
+  set.seed(seed)
+
+  # number of observations
+  nobs <- obs
+
+  # random data
+  data <- tibble::tibble(
+    age = sample(16:95, nobs, replace = TRUE),
+    gender = sample(c("Male","Female", "X"), prob = c(0.49, 0.49, 0.02), nobs, replace = TRUE),
+    eye_color = sample(c("blue","green","brown"), nobs, replace = TRUE),
+    shoe_size = trunc(rnorm(nobs, mean = 43, sd = 3)),
+    iq = trunc(rnorm(nobs, mean = 100, sd = 20)),
+    education = sample(c(0:100), nobs, replace = TRUE),
+    income = sample(c(0:100), nobs, replace = TRUE),
+
+    handset = sample(c("Apple","Android", "Other"), prob = c(0.4,0.5, 0.1), nobs, replace = TRUE),
+
+    pet = sample(c("dog","cat","other","no"),
+                 prob = c(0.23,0.22,0.11,0.35),
+                 nobs, replace = TRUE),
+
+    favorite_pizza = sample(c("Margaritha", "Carciofi","Pepperoni", "Hawai", "Quattro Statgioni", "Provenciale"), nobs, replace = TRUE),
+    favorite_icecream = sample(c("Vanilla", "Chocolate","Strawberry", "Lemon", "Cookie", "Hazelnut","Apple"),
+                               prob=c(0.3,0.2,0.2,0.1,0.1,0.05,0.05),
+                               nobs, replace = TRUE),
+    likes_garlic = sample(0:1, nobs, prob = c(0.4, 0.6), replace = TRUE),
+    likes_sushi = sample(0:1, nobs, prob = c(0.5, 0.5), replace = TRUE),
+    likes_beatles = sample(0:1, nobs, prob = c(0.6, 0.4), replace = TRUE),
+    likes_beer = sample(0:1, nobs, prob = c(0.6, 0.4), replace = TRUE)
+  )
+
+  # not random correlations
+  random01 <- runif(nobs)
+  data$likes_beatles <- ifelse(data$likes_beatles + data$age/50 * random01>= 1, 1, 0)
+  data$likes_beer <- ifelse(data$gender == "Male" & random01 >= 0.2, 1, data$likes_beer)
+  data$likes_sushi <- ifelse(data$age/75 + random01/4 >= 1, 0, data$likes_sushi)
+  data$shoe_size <- ifelse(data$gender == "Female"  & random01 >= 0.3, data$shoe_size - 1.8, data$shoe_size)
+  data$income <- ifelse(data$education > 50 & random01 > 0.3, data$income + data$education/2, data$income)
+  data$handset <- ifelse(data$handset == "Android" & data$income>75 & data$education<25, "Apple", data$handset)
+  # add extreme values
+  #data[1,"age"] <- 5000
+
+  # return data
+  data
+
+} #create_data_person
+
+
+#' Create data buy
+#'
+#' Artificial data that can be used for unit-testing or teaching
+#'
+#' @param obs Number of observations
+#' @param target_name Variable name of target
+#' @param factorise_target Should target variable be factorised?
+#' (from 0/1 to facotr no/yes)?
+#' @param target1_prob Probability that buy = 1
+#' @param add_extreme Add an obervation with extreme values?
+#' @param flip_gender Should Male/Female be flipped in data?
+#' @param add_id Add an id-variable to data?
+#' @param seed Seed for randomization
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @md
+#' @details
+#' Variables in dataset:
+#' * id = Identifier
+#' * period = Year & Month (YYYYMM)
+#' * city_ind = Indicating if customer is residing in a city (1 = yes, 0 = no)
+#' * female_ind = Gender of customer is female (1 = yes, 0 = no)
+#' * fixedvoice_ind = Customer has a fixed voice product (1 = yes, 0 = no)
+#' * fixeddata_ind = Customer has a fixed data product (1 = yes, 0 = no)
+#' * fixedtv_ind = Customer has a fixed tv product (1 = yes, 0 = no)
+#' * mobilevoice_ind = Customer has a mobile voice product (1 = yes, 0 = no)
+#' * mobiledata_ind = Customer has a mobile data product (1 = yes, 0 = no)
+#' * bbi_speed_ind = Customer has a Broadband Internet (BBI) with extra speed
+#' * bbi_usg_gb = Broadband Internet (BBI) usage in Gigabyte (GB) last month
+#' * hh_single = Expected to be a Single Household (1 = yes, 0 = no)
+#'
+#' Target in dataset:
+#' * buy (may be renamed) = Did customer buy a new product in next month?
+#' (1 = yes, 0 = no)
+
+
+create_data_buy = function(obs = 1000,
+                           target_name = "buy",
+                           factorise_target = FALSE,
+                           target1_prob = 0.5,
+                           add_extreme = TRUE,
+                           flip_gender = FALSE,
+                           add_id = FALSE,
+                           seed = 123) {
+
+  # define variables for CRAN-package check
+  target_ind <- NULL
+  age <- NULL
+  city_ind <- NULL
+
+  # set seed (randomization)
+  set.seed(seed)
+
+  # create basic dataset
+  data <- data.frame(
+    id = seq(from = 1, to = obs),
+    period = rep(202012, obs),
+    target_ind = sample(c(0, 1),
+                        obs,
+                        prob = c(1 - target1_prob, target1_prob),
+                        replace = TRUE)
+  )
+
+  # add features
+  data <- data %>%
+    dplyr::mutate(
+      age = round(ifelse(target_ind == 1,
+                         stats::rnorm(obs, mean = 45, sd = 10),
+                         stats::rnorm(obs, mean = 60, sd = 10)
+      ), 0),
+      city_ind = ifelse(target_ind == 1,
+                        sample(c(0, 1), obs, replace = TRUE, prob = c(0.4, 0.6)),
+                        sample(c(0, 1), obs, replace = TRUE, prob = c(0.6, 0.4))
+      ),
+      female_ind = ifelse(target_ind == 1,
+                          sample(c(0, 1), obs, replace = TRUE, prob = c(0.3, 0.7)),
+                          sample(c(0, 1), obs, replace = TRUE, prob = c(0.7, 0.3))
+      ),
+      fixedvoice_ind = ifelse(age > 70,
+                              sample(c(0, 1), obs, replace = TRUE, prob = c(0.3, 0.7)),
+                              sample(c(0, 1), obs, replace = TRUE, prob = c(0.95, 0.05))
+      ),
+      fixeddata_ind = 1,
+      fixedtv_ind = ifelse(target_ind == 1,
+                           sample(c(0, 1), obs, replace = TRUE, prob = c(0.4, 0.6)),
+                           sample(c(0, 1), obs, replace = TRUE, prob = c(0.8, 0.2))
+      ),
+      mobilevoice_ind = sample(c(0, 1), obs, replace = TRUE, prob = c(0.4, 0.6)),
+      mobiledata_ind = sample(c("NO","MOBILE STICK", "BUSINESS"), obs, replace = TRUE, prob = c(0.5, 0.3, 0.2)),
+      bbi_speed_ind = ifelse(age > 60,
+                             sample(c(0, 1), obs, replace = TRUE, prob = c(0.9, 0.1)),
+                             sample(c(0, 1), obs, replace = TRUE, prob = c(0.2, 0.8))
+      ),
+      bbi_usg_gb = ifelse(age > 75,
+                          round(stats::rnorm(obs, mean = 10, sd = 1)),
+                          round(stats::rnorm(obs, mean = 50, sd = 10))
+      ) + city_ind * 20 + target_ind * 10,
+      hh_single = ifelse(age < 35 & city_ind == 1,
+                         sample(c(0, 1), obs, replace = TRUE, prob = c(0.2, 0.8)),
+                         sample(c(0, 1), obs, replace = TRUE, prob = c(0.7, 0.3))
+      )
+    ) # mutate
+
+  # add extreme values?
+  if (add_extreme) {
+    extreme <- data[nrow(data), ]
+    extreme$bbi_usg_gb <- 100000
+    extreme$target_ind <- 0
+    data[nrow(data), ] <- extreme[1, ]
+  }
+
+  # factorise target?
+  if (factorise_target) {
+    data$target_ind <- factor(data$target_ind,
+                              levels = c(0, 1),
+                              labels = c("no", "yes"))
+  }
+
+  # rename target?
+  if (target_name != "target_ind") {
+    names(data)[[grep("target_ind", names(data))]] <- target_name
+  }
+
+  # flip gender?
+  if (flip_gender) {
+    data[["female_ind"]] <- ifelse(data[["female_ind"]] == 1, 0, 1)
+  }
+
+  # add id?
+  if (!add_id)  {
+    data$id <- NULL
+  }
+
+  # return data
+  data
+
+} # create_data_buy
+
+#' Create data random
+#'
+#' Random data that can be used for unit-testing or teaching
+#'
+#' @param obs Number of observations
+#' @param vars Number of variables
+#' @param target_name Variable name of target
+#' @param factorise_target Should target variable be factorised?
+#' (from 0/1 to facotr no/yes)?
+#' @param target1_prob Probability that buy = 1
+#' @param add_id Add an id-variable to data?
+#' @param seed Seed for randomization
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @md
+#' @details
+#' Variables in dataset:
+#' * id = Identifier
+#' * var_X = variable containing values between 0 and 100
+#'
+#' Target in dataset:
+#' * target_ind (may be renamed) = random values (1 = yes, 0 = no)
+
+create_data_random = function(obs = 1000, vars = 10,
+                              target_name = "target_ind",
+                              factorise_target = FALSE,
+                              target1_prob = 0.5,
+                              add_id = TRUE,
+                              seed = 123) {
+
+  # set seed (randomization)
+  set.seed(seed)
+
+  # create basic dataset
+  data <- data.frame(
+    id = seq(from = 1, to = obs),
+    target_ind = sample(c(0, 1),
+                        obs,
+                        prob = c(1 - target1_prob, target1_prob),
+                        replace = TRUE)
+  )
+
+  # add features
+  for (i in seq_len(vars)) {
+    data[[paste0("var_",i)]] <- as.integer(
+      round(stats::runif(obs)*100,0))
+  }
+
+
+  # factorise target?
+  if (factorise_target) {
+    data$buy <- factor(data$buy,
+                       levels = c(0, 1),
+                       labels = c("no", "yes"))
+  }
+
+  # rename target?
+  if (target_name != "target_ind") {
+    data[[target_name]] <- data$target_ind
+    data$target_ind <- NULL
+  }
+
+  # add id?
+  if (!add_id)  {
+    data$id <- NULL
+  }
+
+  # return data
+  data
+
+} # create_data_random
