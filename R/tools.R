438f81b341c55571b35a8f6377cf0c2cb925ca62
olivroy <olivierroy71@hotmail.com>
2023-10-10 10:45:37 -0400

    Mark `get_nrow()` with `@keywords internal`, so it doesn't show up on the function index. (while being still exported)

--- R/tools.R
+++ R/tools.R
@@ -489,12 +489,15 @@ guess_cat_num <- function(var, descr)  {
 } # guess_cat_num
 
 
-#' Get number of rows for a grid plot (deprecated, use total_fig_height() instead)
+#' Get number of rows for a grid plot
+#'
+#' This function is deprecated, please use [total_fig_height()] instead.
 #'
 #' @param varnames List of variables to be plotted
 #' @param exclude Number of variables that will be excluded from plot
 #' @param ncol Number of columns (default = 2)
 #' @return Number of rows
+#' @keywords internal
 #' @examples
 #' get_nrow(names(iris), ncol = 2)
 #' @export
